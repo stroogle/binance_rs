8978a2f22fc0d90ae2e10c133bea25393571b2a7
Colm Moore <contact@colm.tech>
2022-12-01 11:36:42 +1100

    Binance struct now connects, listens, pongs and resets its connection. Can Symbol can successfully update and calculcate trades.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,24 @@
 //! A Rust library build to interact with the Binance API
+
+// Modules
+mod symbol;
+
+// Uses
+use tungstenite::{WebSocket, Message};
+use tungstenite::stream::MaybeTlsStream;
+use std::net::TcpStream;
+use url::Url;
+use tungstenite::{connect};
+use std::time;
+use symbol::Symbol;
+
+const BINANCE_WS_URL: &str = "wss://stream.binance.com:443/ws";
+
 pub struct Binance {
-    api_key: String
+    _api_key: String,
+    pub connection: Option<WebSocket<MaybeTlsStream<TcpStream>>>,
+    pub connection_began_at: time::Instant,
+    pub symbols: Option<Vec<Symbol>>
 }
 
 impl Binance {
@@ -12,32 +30,71 @@ impl Binance {
     /// ```
     pub fn new(api_key: &str) -> Binance {
         Binance {
-            api_key: api_key.to_string()
+            _api_key: api_key.to_string(),
+            connection: None,
+            connection_began_at: time::Instant::now(),
+            symbols: None
         }
     }
 
     /// Creates a new connection and attaches it to instance
-    pub fn new_connection() {}
+    pub fn new_connection(&mut self) {
+        let (socket, _response) = connect(
+            Url::parse(BINANCE_WS_URL).unwrap()
+        ).expect("Couldn't connect to socket");
+        self.connection_began_at = time::Instant::now();
+        self.connection = Some(socket);
+        self.follow_ticker("ETHBUSD");
+    }
 
-    /// Listens for connection data stream
+    /// Not a completed function yet.
+    /// Used to subscribe to tickers on the socket connection.
+    pub fn follow_ticker(&mut self, _ticker: &str) {
+        if self.connection.is_none() {
+            return;
+        }
+        self.connection.as_mut().unwrap().write_message(Message::Text(r#"{
+            "method": "SUBSCRIBE",
+            "params":
+            [
+                "ethusdt@bookTicker",
+                "bnbeth@bookTicker",
+                "etceth@bookTicker",
+                "zeceth@bookTicker"
+            ],
+            "id": 1
+        }"#.into())).expect("Uh oh!");
+    }
+
+    /// Handles incoming data stream.
+    /// Callback function only called on incoming ticker data.
+    /// connection resets handled automatically in here.
     /// 
     /// ```
     /// let x: Binance = Binance::new("<api_key>");
     /// let add = |a, b| => a + b;
     /// x.connect(add);
     /// ```
-    pub fn connect(&self, callback: fn()) {
-        callback();
-    }
+    pub fn handle_incoming(&mut self, callback: fn(s: &str)) {
+        loop {
+            let msg: Message = self.connection.as_mut().unwrap().read_message().expect("Error reading message");
+            let msg_as_string = msg.to_string();
 
-    /// Responds to ping message
-    pub fn send_pong() {
+            // Handle ping
+            // Handle incoming ticker data
+            callback(&msg_as_string);
 
+            // Reset the connection if it has been running for 12 hours
+            if self.connection_began_at.elapsed().as_secs() > (60 * 60 * 12) {
+                self.reset_connection();
+            }
+        }
     }
 
     /// Resets connection
-    pub fn reset_connection() {
-
+    pub fn reset_connection(&mut self) {
+        println!("Connection being reset.");
+        self.new_connection();
     }
 }
 
@@ -45,4 +102,14 @@ impl Binance {
 mod tests {
     use super::*;
 
+    #[test]
+    pub fn test_incoming_message() {
+        let mut b1 = Binance::new("Bruh Moment");
+
+        b1.new_connection();
+        fn print_msg(s: &str) {
+            println!("{}", &s);
+        }
+        b1.handle_incoming(print_msg);
+    }
 }
---
+++ src/symbol.rs
@@ -0,0 +1,135 @@
+//! Symbols is used to interact with specific trading symbols on the Binance spot market
+
+use serde_json::{Value};
+use rust_decimal::prelude::*;
+use std::cmp::Ordering;
+
+pub enum Side {
+    Base,
+    Quote
+}
+
+/// Represets a trading symbol on the binance spot trading market.
+pub struct Symbol {
+    pub base: String,
+    pub quote: String,
+    pub asking_price: String,
+    pub asking_qty: String,
+    pub bid_price: String,
+    pub bid_qty: String
+}
+
+impl Symbol {
+    pub fn new(base: &str, quote: &str) -> Self {
+
+        Self {
+            base: base.to_string(),
+            quote: quote.to_string(),
+            asking_price: "1.000000".to_string(),
+            asking_qty: "-1.000000".to_string(),
+            bid_price: "1.000000".to_string(),
+            bid_qty: "-1.000000".to_string()
+        }
+
+    }
+
+    pub fn calculate_trade(&self, owned_asset: Side, amount: &str) -> Result<String, &str> {
+        let owned_amount = Decimal::from_str(amount).unwrap();
+        let res: Decimal;
+        match owned_asset {
+            Side::Base => {
+                let bid_price = Decimal::from_str(&self.bid_price).unwrap();
+                res = owned_amount * bid_price;
+                let bid_qty = Decimal::from_str(&self.bid_qty).unwrap();
+                let total_value = bid_price * bid_qty;
+                if res.cmp(&total_value) == Ordering::Greater {
+                    return Err("Base_side Not enough qty to trade");
+                }
+            },
+            Side::Quote => {
+                res = owned_amount / Decimal::from_str(&self.asking_price).unwrap();
+                let ask_qty = Decimal::from_str(&self.asking_qty).unwrap();
+                if res.cmp(&ask_qty) == Ordering::Greater {
+                    return Err("Not enough qty to trade");
+                }
+            }
+        }
+        Ok(res.round_dp_with_strategy(8, RoundingStrategy::ToZero).to_string())
+    }
+
+    pub fn update(&mut self, json_str: &str) {
+        let res: Value = serde_json::from_str(json_str).unwrap();
+
+        self.asking_price = res["a"].to_string().replace('\"', "");
+        self.asking_qty= res["A"].to_string().replace('\"', "");
+        self.bid_price= res["b"].to_string().replace('\"', "");
+        self.bid_qty= res["B"].to_string().replace('\"', "");
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let s1 = Symbol::new("BTC", "USDT");
+
+        assert_eq!(s1.base, "BTC");
+        assert_eq!(s1.quote, "USDT");
+
+        assert_eq!(s1.asking_price, "1.000000");
+        assert_eq!(s1.asking_qty, "1.000000");
+        assert_eq!(s1.bid_price, "1.000000");
+        assert_eq!(s1.bid_qty, "1.000000");
+    }
+
+    #[test]
+    fn test_calculate_trade_base() {
+        let json_string = r#"{"u":22277893334,"s":"ETHUSDT","b":"1268.53000000","B":"107.76630000","a":"1268.54000000","A":"3.89930000"}"#;
+        let mut s1 = Symbol::new("ETH", "USDT");
+        s1.update(json_string);
+
+        assert_eq!(s1.calculate_trade(Side::Base, "100").unwrap(), "126853.00000000");
+        assert_eq!(s1.calculate_trade(Side::Base, "10").unwrap(), "12685.30000000");
+        assert_eq!(s1.calculate_trade(Side::Base, "1").unwrap(), "1268.53000000");
+        assert_eq!(s1.calculate_trade(Side::Base, "107.76630000").unwrap(), "136704.78453900");
+    
+        assert_eq!(s1.calculate_trade(Side::Base, "107.76640000"), Err("Base_side Not enough qty to trade"));
+
+        let s2 = Symbol::new("ETH", "USDT");
+
+        assert_eq!(s2.calculate_trade(Side::Base, "1"), Err("Base_side Not enough qty to trade"));        
+        assert_eq!(s2.calculate_trade(Side::Quote, "100"), Err("Not enough qty to trade"));        
+    }
+     #[test]
+    fn test_calculate_trade_quote() {
+        let json_string = r#"{"u":22277893334,"s":"ETHUSDT","b":"1268.53000000","B":"107.76630000","a":"1268.54000000","A":"3.89930000"}"#;
+        let mut s1 = Symbol::new("ETH", "USDT");
+        s1.update(json_string);
+
+        assert_eq!(s1.calculate_trade(Side::Quote, "100").unwrap(), "0.07883078");
+        assert_eq!(s1.calculate_trade(Side::Quote, "10").unwrap(), "0.00788307");
+        assert_eq!(s1.calculate_trade(Side::Quote, "1").unwrap(), "0.00078830");
+        assert_eq!(s1.calculate_trade(Side::Quote, "4946.418022").unwrap(), "3.8993");
+    
+        assert_eq!(s1.calculate_trade(Side::Quote, "4946.418023"), Err("Not enough qty to trade"));
+    }
+
+    #[test]
+    fn test_update() {
+        let json_string = r#"{"u":22277893334,"s":"ETHUSDT","b":"1268.53000000","B":"107.76630000","a":"1268.54000000","A":"3.89930000"}"#;
+        let mut s1 = Symbol::new("ETH", "USDT");
+        s1.update(json_string);
+
+        assert_eq!(s1.base, "ETH");
+        assert_eq!(s1.quote, "USDT");
+
+        assert_eq!(s1.bid_price, "1268.53000000");
+        assert_eq!(s1.bid_qty, "107.76630000");
+        assert_eq!(s1.asking_price, "1268.54000000");
+        assert_eq!(s1.asking_qty, "3.89930000");
+    }
+
+}
